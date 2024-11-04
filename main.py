8508525d52be1e7e78e1481592b5905cb355fc15
Ronifiu <aron.pakozdi@gmail.com>
2024-11-04 21:48:35 +0100

    finished project

--- main.py
+++ main.py
@@ -1,3 +1,15 @@
+def main():
+    book_path = "books/frankenstein.txt"
+    text = get_book_text(book_path)
+    word_count = count_words(text)
+    chars = count_characters(text)
+    report_print(word_count, chars, book_path)
+    
+
+def get_book_text(book_path):
+    with open(book_path) as f:
+        return f.read()
+
 def count_words(file):
     word_list = file.split()
     word_count = len(word_list)
@@ -14,9 +26,15 @@ def count_characters(file):
             character_hash[letter] = 1
     return character_hash  
 
+def report_print(word_count, character_hash, book_path):
+    sorted_character_hash = dict(sorted(character_hash.items(), key = lambda item: item[1], reverse=True))
+    print(f"--- Begin report of {book_path} ---")
+    print(f"{word_count} words found in the document")
+    print("")
+    for char in sorted_character_hash:
+        if char.isalpha():
+            print(f"The '{char}' character was found {sorted_character_hash[char]} times")
+    print("--- End report ---")
 
-with open("books/frankenstein.txt") as f:
-    file_contents = f.read()
-    print(file_contents)
-    print(count_words(file_contents))
-    print(count_characters(file_contents))
+main()
+        
